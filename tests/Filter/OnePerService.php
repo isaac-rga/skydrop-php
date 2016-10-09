7e670f3ea41a7059e5053efcae3028a8cbad4c3c
Yaser Almasri <yaser@masys.co>
2016-10-09 18:37:52 -0500

    fix errors, add specs

--- tests/Filter/OnePerService.php
+++ tests/Filter/OnePerService.php
@@ -3,16 +3,27 @@ use PHPUnit\Framework\TestCase;
 
 class OnePerServiceTest extends TestCase
 {
-    public function testSetApiKey()
+    public function testReturnSameDayRates()
     {
         $json_rates = file_get_contents(__DIR__.'/../fixtures/rates.json');
         $rates = json_decode($json_rates)->rates;
-        // var_dump($rates);
         $klass = new \Skydrop\ShippingRate\Filter\OnePerService(
             $rates, ['serviceTypes' => ['Hoy']]
         );
         $newRates = $klass->call();
 
         $this->assertEquals(count($newRates), 1);
+    }
+
+    public function testReturnAllRates()
+    {
+        $json_rates = file_get_contents(__DIR__.'/../fixtures/rates.json');
+        $rates = json_decode($json_rates)->rates;
+        $klass = new \Skydrop\ShippingRate\Filter\OnePerService(
+            $rates, ['serviceTypes' => ['Hoy', 'EExps', 'next_day']]
+        );
+        $newRates = $klass->call();
+
+        $this->assertEquals(count($newRates), 4);
     }
 }
