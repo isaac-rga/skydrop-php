362c8e6efad92075255b9a114e5915d79d3e9ad3
colin <acolin@incaztech.com>
2016-10-12 14:51:31 -0500

    use array merge for conditional json generation

--- Skydrop/Order/AbstractOrderAdapter.php
+++ Skydrop/Order/AbstractOrderAdapter.php
@@ -10,8 +10,8 @@ abstract class AbstractOrderAdapter
     public $service;
     public $package;
 
-    abstract protected function originClass();
-    abstract protected function destinationClass();
+    abstract protected function pickupClass();
+    abstract protected function deliveryClass();
     abstract protected function serviceClass();
     abstract protected function packageClass();
 
@@ -20,20 +20,25 @@ abstract class AbstractOrderAdapter
         $serviceClass = $this->serviceClass();
         $packageClass = $this->packageClass();
         $this->args =  $args;
-        $this->origin = initAddress('origin');
-        $this->destination = initAddress('destination');
+        $this->origin = initAddress('pickup');
+        $this->destination = initAddress('delivery');
         $this->service = new $serviceClass($args);
         $this->package = new $packageClass($args);
     }
 
     public function toJson()
     {
-        return array(
-            'pickup' => addressHash($origin),
-            'delivery' => addressHash($destination),
-            'service' => serviceHash(),
-            'package' => packageHash(),
+        return array_merge(
+            addressHash($origin, 'origin'),
+            addressHash($destination, 'destination'),
+            serviceHash(),
+            packageHash()
         );
+    }
+
+    protected function needsCashOnDelivery()
+    {
+        return false;
     }
 
     private function initAddress($category)
@@ -43,34 +48,43 @@ abstract class AbstractOrderAdapter
     }
 
 
-    private function addressHash($address)
+    private function addressHash($address, $category)
     {
         return array(
-            'name' => $address->name(),
-            'email' => $address->email(),
-            'street_name_and_number' => $address->streetNameAndNumber(),
-            'municipality' => $address->municipality(),
-            'neighborhood' => $address->neighborhood(),
-            'telephone' => $address->telephone(),
-        );
+            "{$category}" => array(
+                'name' => $address->name(),
+                'email' => $address->email(),
+                'street_name_and_number' => $address->streetNameAndNumber(),
+                'municipality' => $address->municipality(),
+                'neighborhood' => $address->neighborhood(),
+                'telephone' => $address->telephone(),)
+            );
     }
 
     private function serviceHash()
     {
         return array(
-            'service_code' => $service->serviceCode(),
-            'vehicle_type' => $service->vehicleType(),
-            'schedule_date' => $service->scheduleDate(),
-            'starting_hour' => $service->startingHour(),
-            'ending_hour' => $service->endingHour(),
+            'service' => array(
+                'service_code' => $service->serviceCode(),
+                'vehicle_type' => $service->vehicleType(),
+                'schedule_date' => $service->scheduleDate(),
+                'starting_hour' => $service->startingHour(),
+                'ending_hour' => $service->endingHour(),
+            )
         );
     }
 
     private function packageHash()
     {
-        return array(
-            'cash_on_delivery' => $package->cashOnDelivery(),
-            'cod_amount' => $package->codAmount(),
-        );
+        if (needsCashOnDelivery()) {
+            return array(
+                'package' => array(
+                    'cash_on_delivery' => $package->cashOnDelivery(),
+                    'cod_amount' => $package->codAmount(),
+                )
+            );
+        }
+
+        return array();
     }
 }
