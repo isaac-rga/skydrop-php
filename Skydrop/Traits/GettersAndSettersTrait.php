f5f285252fccd6c12a1b554ddbbade322ebc9d2a
Yaser Almasri <yaser@masys.co>
2016-10-13 14:20:23 -0500

    add GettersAndSetters class

---
+++ Skydrop/Traits/GettersAndSettersTrait.php
@@ -0,0 +1,16 @@
+<?php
+trait GettersAndSettersTrait
+{
+   function __call($method, $params) {
+
+       $var = lcfirst(substr($method, 3));
+
+       if (substr($method, 0, 3) == 'get') {
+           return $this->$var;
+       }
+
+       if (substr($method, 0, 3) == 'set') {
+           $this->$var = $params[0];
+       }
+   }
+}
